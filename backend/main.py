85a50446354c82a93c74df6bac5873488b99fd49
k4noise <vestertp@gmail.com>
2026-02-19 18:27:41 +0500

    init(backend): fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Привет!"}
+
+@app.get("/ping")
+async def ping():
+    return {"status": "ok", "answer": "pong"}
+
+def run_dev():
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+def run_prod():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
